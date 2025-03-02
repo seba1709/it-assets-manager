5a121d0254e40cbd66e45ff91bf3192e6d5d10ee
SP_browneye <seba1709@gmail.com>
2025-03-02 20:35:17 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
+from flask import Flask
+import os
+import psycopg2
+
+app = Flask(__name__)
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+@app.route("/")
+def home():
+    return "IT Assets Manager API is running!"
+
+if __name__ == "__main__":
+    app.run(debug=True)
  
