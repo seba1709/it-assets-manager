488e9eb7d708cf5d618c6ac4d5900be84252ca22
SP_browneye <seba1709@gmail.com>
2025-03-02 20:40:07 +0100

    Update celery_worker.py

--- celery_worker.py
+++ celery_worker.py
@@ -1,1 +1,13 @@
+from celery import Celery
+import os
+
+app = Celery(
+    'tasks',
+    broker=os.getenv("REDIS_URL", "redis://localhost:6379/0"),
+    backend=os.getenv("REDIS_URL", "redis://localhost:6379/0")
+)
+
+@app.task
+def test_task():
+    return "Celery is working!"
 
